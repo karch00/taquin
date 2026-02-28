3e48b069e09514616e5871c756fea7216c648ce8
karch <karch00@proton.me>
2026-02-28 02:10:44 +0100

    Updated minimal terminal size

--- include/constants.h
+++ include/constants.h
@@ -1,7 +1,7 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-const int MIN_TERMSIZE[2] = {35, 92}; // Rows, Columns
+const int MIN_TERMSIZE[2] = {50, 150}; // Rows, Columns
 const int TITLE_SIZE[2] = {8, 62};
 const int OPTIONS_SIZE[2] = {4, 10};
 const int CONTROLS_SIZE[2] = {23, 45};
